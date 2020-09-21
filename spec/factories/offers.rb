144ed4399c8de611e12014f75bcbf8744cb32ddc
Diego Novais <contato@diegonovais.com.br>
2020-09-21 01:10:50 -0300

    Fixing intermintent error

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -3,24 +3,24 @@ FactoryBot.define do
     advertiser_name { Faker::Company.name }
     url { Faker::Internet.url }
     description { Faker::Lorem.sentences(number: 1) }
-    starts_at { Faker::Time.between_dates(from: Date.today - 2, to: Date.today - 1) }
-    ends_at { Faker::Time.between_dates(from: Date.today + 1, to: Date.today + 2) }
+    starts_at { Faker::Time.between_dates(from: DateTime.now - 2, to: DateTime.now - 1) }
+    ends_at { Faker::Time.between_dates(from: DateTime.now + 1, to: DateTime.now + 2) }
     premium { false }
 
     trait :started do
-      starts_at { Faker::Time.between_dates(from: Date.today - 2, to: Date.today - 1) }
-      ends_at { Faker::Time.between_dates(from: Date.today + 1, to: Date.today + 2) }
+      starts_at { Faker::Time.between_dates(from: DateTime.now - 2, to: DateTime.now - 1) }
+      ends_at { Faker::Time.between_dates(from: DateTime.now + 1, to: DateTime.now + 2) }
       state { 'enabled' }
     end
 
     trait :finished do
-      starts_at { Faker::Time.between_dates(from: Date.today - 5, to: Date.today - 4) }
-      ends_at { Faker::Time.between_dates(from: Date.today - 2, to: Date.today - 1) }
+      starts_at { Faker::Time.between_dates(from: DateTime.now - 5, to: DateTime.now - 4) }
+      ends_at { Faker::Time.between_dates(from: DateTime.now - 2, to: DateTime.now - 1) }
       state { 'disabled' }
     end
 
     trait :not_expires do
-      starts_at { Faker::Time.between_dates(from: Date.today - 2, to: Date.today) }
+      starts_at { Faker::Time.between_dates(from: DateTime.now - 2, to: DateTime.now) }
       ends_at { '' }
     end
 
